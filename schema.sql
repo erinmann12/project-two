49d46510bebaf6625b9f37376ef0e46fdc250cdc
Erin Coates Mann <erin.mann2019@gmail.com>
2021-04-10 12:13:07 -0700

    pushed to database

--- schema.sql
+++ schema.sql
@@ -1,6 +1,38 @@
--- CREATE TABLE hair (
---     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
---     rockband VARCHAR(100) NOT NULL,
---     num_hits INTEGER NOT NULL,
---     num_albums INTEGER NOT NULL,
--- );
+CREATE TABLE state (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    hpi_type VARCHAR(100) NOT NULL,
+    purchase_type VARCHAR(100) NOT NULL,
+    frequency VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    place_name VARCHAR(100) NOT NULL,
+    place_id VARCHAR(100) NOT NULL,
+    year INTEGER NOT NULL,
+    period INTEGER NOT NULL,
+    price INTEGER NOT NULL
+);
+
+CREATE TABLE usa (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    hpi_type VARCHAR(100) NOT NULL,
+    purchase_type VARCHAR(100) NOT NULL,
+    frequency VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    place_name VARCHAR(100) NOT NULL,
+    place_id VARCHAR(100) NOT NULL,
+    year INTEGER NOT NULL,
+    period INTEGER NOT NULL,
+    price INTEGER NOT NULL
+);
+
+CREATE TABLE puerto_rico (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    hpi_type VARCHAR(100) NOT NULL,
+    purchase_type VARCHAR(100) NOT NULL,
+    frequency VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    place_name VARCHAR(100) NOT NULL,
+    place_id VARCHAR(100) NOT NULL,
+    year INTEGER NOT NULL,
+    period INTEGER NOT NULL,
+    price INTEGER NOT NULL
+);
